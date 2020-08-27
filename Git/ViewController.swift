d03ca1da5d9e4a57be6b5057755b1358460aa4d0
Stanislav Leontyev <arbuspilot@gmail.com>
2020-08-27 16:24:21 +0400

    Initial commit

---
+++ Git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git
+//
+//  Created by Stanislav Leontyev on 27.08.2020.
+//  Copyright © 2020 Stanislav Leontyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
